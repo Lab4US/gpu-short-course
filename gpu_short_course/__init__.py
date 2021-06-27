9598a6b613745ad42f42d33c93731f3ed3bf2f8d
Piotr Jarosik <jarosik.piotr@gmail.com>
2021-06-27 12:24:32 +0200

    Print available gpus on start.

--- gpu_short_course/__init__.py
+++ gpu_short_course/__init__.py
@@ -1,3 +1,18 @@
+import cupy as cp
+
 from gpu_short_course.tests import (
-   convolve_2d_input
+    convolve_2d_input
 )
+
+
+def print_device_info():
+    n_gpus = cp.cuda.runtime.getDeviceCount()
+    if n_gpus == 0:
+        print("NO GPU AVAILABLE.")
+    else:
+        for i in range(n_gpus):
+            device_props = cp.cuda.runtime.getDeviceProperties(i)
+            print(f"GPU:{i}: {device_props['name']}")
+
+
+print_device_info()
