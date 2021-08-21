e105848e0c0d061b94ac2778c8f9c2aeb6cad40c
Piotr Jarosik <jarosik.piotr@gmail.com>
2021-08-21 12:28:49 +0200

    Restored tools.py module.

---
+++ gpu_short_course/tools.py
@@ -0,0 +1,15 @@
+import cupy as cp
+
+
+def register_host_buffer(buffer):
+    for element in buffer:
+        ptr = element.ctypes.data
+        nbytes = element.nbytes
+        cp.cuda.runtime.hostRegister(ptr, nbytes, 1)
+
+
+def unregister_host_buffer(buffer):
+    for elements in buffer:
+        ptr = element.ctypes.data
+        cp.cuda.runtime.hostUnregister(ptr)
+
