1f231aa0ab73f42ca1a1a8c9c68b5ba3f0b51670
Piotr Jarosik <jarosik.piotr@gmail.com>
2021-08-22 19:17:03 +0200

    Repository structure refactoring.

--- utils/setup.py
+++ utils/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='gpu_short_course',
-      version='0.0.1',
+      version='1.0.0',
       description='GPU short course',
       author='us4us Ltd.',
       author_email='support@us4us.eu',
